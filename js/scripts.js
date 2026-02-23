a6b44b61c2155585c47331567c1aaced2666560b
Marcelo Dias <m4rcelodias@live.com>
2026-02-22 23:19:02 -0300

    feat: Primeiras telas criadas (MVP) - tela de login; - tela da cartela de aposta; - arquivos css e js

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+    $('.modal').modal();
+
+    const TOTAL_JOGOS_RODADA = 2;
+
+    $('.btn-checkbox-x').on('click', function() {
+        let $cardDoJogo = $(this).closest('.jogo-card-novo');
+        $cardDoJogo.find('.btn-checkbox-x').removeClass('ativo');
+        $(this).addClass('ativo');
+        verificarCartelaCompleta();
+    });
+
+    function verificarCartelaCompleta() {
+        let palpitesFeitos = $('.jogo-card-novo').filter(function() {
+            return $(this).find('.btn-checkbox-x.ativo').length > 0;
+        }).length;
+
+        $('#contador-palpites').text(`${palpitesFeitos} / ${TOTAL_JOGOS_RODADA} preenchidos`);
+
+        if (palpitesFeitos === TOTAL_JOGOS_RODADA) {
+            $('#btn-salvar-cartela').prop('disabled', false).removeClass('green darken-2').addClass('orange darken-3');
+        } else {
+            $('#btn-salvar-cartela').prop('disabled', true).removeClass('orange darken-3').addClass('green darken-2');
+        }
+    }
+});
